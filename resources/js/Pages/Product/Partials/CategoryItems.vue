177547486dccf0b989834788bf2b436ee5ac0f34
Terence Ralph <terenceralph.ragaas@gmail.com>
2024-06-06 18:02:21 +0800

    add modal for Products

--- resources/js/Pages/Product/Partials/CategoryItems.vue
+++ resources/js/Pages/Product/Partials/CategoryItems.vue
@@ -1,6 +1,7 @@
 <script setup>
-import PrimaryButton from "@/Components/PrimaryButton.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
+import DisplayProduct from "./DisplayProduct.vue";
+import SuccessButton from "@/Components/SuccessButton.vue";
 const values = defineProps(["items", "category"]);
 
 function hasCategory(category) {
@@ -27,17 +28,31 @@ const filteredItems = values.items.filter(
         class="p-6 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 space-y-3 mt-5"
     >
         <div class="shrink-0">
-            <img class="h-16 w-16" :src="item.image" alt="ChitChat Logo" />
+            <img class="h-16 w-16" :src="item.image" alt="Items" />
         </div>
         <div class="flex flex-col">
             <div class="text-xl font-medium text-black">
-                {{ item.title }}
+                <DisplayProduct
+                    :title="item.title"
+                    :image="item.image"
+                    :description="item.description"
+                    class="max-w-sm"
+                ></DisplayProduct>
             </div>
             <p class="text-slate-500 line-clamp-1">
                 {{ item.description }}
             </p>
-            <div class="ml-auto mt-8 space-x-3">
-                <PrimaryButton>Buy Now</PrimaryButton>
+            <div class="flex flex-col mt-8">
+                <div class="text-gray-500 font-bold items-center text-lg">
+                    Cost: ${{ item.price }}
+                </div>
+                <div class="text-gray-400 font-bold items-center text-lg">
+                    Available Unit:{{ item.rating.count }}
+                </div>
+            </div>
+
+            <div class="flex gap-x-4 flex-grow-0 justify-end">
+                <SuccessButton>Buy Now</SuccessButton>
                 <SecondaryButton class="">Add to Cart</SecondaryButton>
             </div>
         </div>
---
+++ resources/js/Pages/Product/Partials/DisplayProduct.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Modal from "@/Components/Modal.vue";
+import SuccessButton from "@/Components/SuccessButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import { ref } from "vue";
+
+const confirmingUserDeletion = ref(false);
+
+defineProps(["title", "description", "image"]);
+const confirmUserDeletion = () => {
+    confirmingUserDeletion.value = true;
+};
+
+const closeModal = () => {
+    confirmingUserDeletion.value = false;
+};
+</script>
+
+<template>
+    <section class="space-y-2">
+        <header>
+            <h2
+                class="text-lg font-medium text-gray-900 hover:cursor-pointer hover:text-sky-500"
+                @click="confirmUserDeletion"
+            >
+                {{ title }}
+            </h2>
+        </header>
+
+        <!-- <DangerButton @click="confirmUserDeletion">Delete Account</DangerButton> -->
+
+        <Modal :show="confirmingUserDeletion" @close="closeModal">
+            <div
+                @click="closeModal"
+                class="text-xl p-5 bg-slate-100 flex font-black justify-end hover:cursor-pointer"
+            >
+                X
+            </div>
+            <div
+                class="p-6 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 space-y-3 mt-5"
+            >
+                <div class="shrink-0">
+                    <img class="h-48 w-48" :src="image" alt="Items" />
+                </div>
+                <div class="flex flex-col space-y-6">
+                    <div class="text-xl font-medium text-black">
+                        {{ title }}
+                    </div>
+                    <p class="text-slate-500">
+                        {{ description }}
+                    </p>
+                    <div class="ml-auto mt-8 space-x-3">
+                        <SuccessButton>Buy Now</SuccessButton>
+                        <SecondaryButton>Add to Cart</SecondaryButton>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    </section>
+</template>
